cb20ed35c316587781b337133a22615d7c348226
07samir07 <samirbirua6738@gmail.com>
2026-03-05 01:15:55 +0530

    fixed all register bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     //file uploaded successfully
-    console.log("file uploaded on cloudinary successfully", response.url);
+    //console.log("file uploaded on cloudinary successfully", response.url);
+    fs.unlinkSync(localFilePath); //remove the file from local storage as we have the file on cloudinary
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); //remove the file from local storage as the operation failed
