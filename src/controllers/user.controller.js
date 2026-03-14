2a586a188d16098cee9ed89be803c9b03488e240
07samir07 <samirbirua6738@gmail.com>
2026-03-14 19:43:00 +0530

    fix: update logoutUser to unset refreshToken and correct subscription check in getUserChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,7 +168,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: { refreshToken: 1 }, //this removes the field from document
     },
     {
       new: true,
@@ -375,7 +375,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         },
         isSubscribed: {
           $cond: {
-            if: { $in: [req.user?._id, "subscribers.subscriber"] },
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
             then: true,
             else: false,
           },
