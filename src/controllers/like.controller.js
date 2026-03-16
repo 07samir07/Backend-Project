e3923bab6f4d803b51ba045821902d6acf3ece95
07samir07 <samirbirua6738@gmail.com>
2026-03-16 13:57:11 +0530

    added getLikedVideos function to fetch liked videos with owner details and improved aggregation pipeline

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -82,4 +82,76 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, { tweetId, isLiked: true }));
 });
 
-export { toggleVideoLike, toggleCommentLike, toggleTweetLike };
+const getLikedVideos = asyncHandler(async (req, res) => {
+  const likedVideosAggregate = await Like.aggregate([
+    {
+      $match: {
+        likedBy: new mongoose.Schema.Types.ObjectId(req.user?._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: likedVideo,
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "ownerDetails",
+            },
+          },
+          {
+            $unwind: "$ownerDetails",
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$likedVideo",
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        likedVideo: {
+          _id: 1,
+          "videoFile.url": 1,
+          "thumbnail.url": 1,
+          owner: 1,
+          title: 1,
+          description: 1,
+          views: 1,
+          duration: 1,
+          createdAt: 1,
+          isPublished: 1,
+          ownerDetails: {
+            username: 1,
+            fullname: 1,
+            "avatar.url": 1,
+          },
+        },
+      },
+    },
+  ]);
+});
+
+return (
+  res.status(200),
+  json(
+    new ApiResponse(
+      200,
+      likedVideosAggregate,
+      "liked videos fetched successfully"
+    )
+  )
+);
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos };
