99087f639ab5b967ad8cbf06bad31da0fb81feb0
07samir07 <samirbirua6738@gmail.com>
2026-03-16 12:23:53 +0530

    added getVideoComments controller to fetch comments for a video with pagination and like details

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Like } from "../models/like.model.js";
+
+//get all comments for a video
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+  const video = Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+  const commentsAggregate = Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Schema.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "owner",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        likesCount: 1,
+        owner: {
+          username: 1,
+          fullname: 1,
+          "avatar.url": 1,
+        },
+        isLiked: 1,
+      },
+    },
+  ]);
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+  const comments = await Comment.aggregatePaginate(commentsAggregate, options);
+
+  return res.status(200).json(200, comments, "Comments fetched successfully");
+});
+
+//add a comment to a video
+
+//update a comment
+
+//delete a comment
+
+export { getVideoComments };
