2e1f2bf84a3ded2704555e8d31ed33730c74a7a4
07samir07 <samirbirua6738@gmail.com>
2026-03-16 13:03:02 +0530

    added addComment, updateComment, and deleteComment functions with error handling in comment.controller.js

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -86,8 +86,91 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 //add a comment to a video
 
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    throw new ApiError(400, "Content id is required");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(500, "Failed to add a comment please try again");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment added successfully"));
+});
+
 //update a comment
 
-//delete a comment
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
 
-export { getVideoComments };
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  if (comment?.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(400, "Only content owner can edit their comment");
+  }
+
+  const updatedComment = await Comment.findByIdAndUpdate(
+    comment?._id,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(500, "Failed to edit comment please try again");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, updatedComment, "Comment edited successfully"));
+});
+
+//delete a comment
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  if (comment?.owner.toString() !== req.user?._id.toString()) {
+    throw new ApiError(400, "Only content owner can delete their comment");
+  }
+
+  await Comment.findByIdAndDelete(commentId);
+  await Like.deleteMany({
+    comment: commentId,
+    likedBy: req.user,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200), { commentId }, "Comment deleted successfully");
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
