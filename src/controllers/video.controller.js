14c00ad1107783fadcffe4bf54beabfb5945e83e
07samir07 <samirbirua6738@gmail.com>
2026-03-18 19:59:55 +0530

    added video publishing and retrieval functionalities with full-text search and user validation

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,9 +1,168 @@
+import mongoose, { isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Video } from "../models/video.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 //GET ALL VIDEOS BASED ON QUERY,SORT AND PAGINATION
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   console.log(userId);
   const pipeline = [];
+
+  //FOR USING FULL TEXT BASED SEARCH U NEED TO CREATE A SEARCH INDEX IN MONGODB ATLAS
+  //YOU CAN INCLUDUE FIELD MAPPINGS IN SEARCH INDEX EG: TTITLE, DESCRIPTION AS WELL
+  //FIELD MAPPINGS SPECIFY WHICH FIELDS WITHIN DOCUMENTS SHOULD BE INDEXED FOR TEXT SEARCH
+  //THIS HELPS IN SEARCHING ONLY IN TITLE, DESCription PROVIDING FASTER SEARCH RESULTS
+  //HERE THHE NAME OF SEARCH INDEX IS 'SEARCH-VIDEOS
+
+  if (query) {
+    pipeline.push({
+      $search: {
+        index: "search-videos",
+        text: {
+          query: query,
+          path: ["title", "description"], //search only on title, description
+        },
+      },
+    });
+  }
+
+  if (userId) {
+    if (!isValidObjectId(userId)) {
+      throw new ApiError(400, "Invalid userId");
+    }
+    pipeline.push({
+      $match: {
+        owner: new mongoose.Schema.Types.ObjectId(userId),
+      },
+    });
+  }
+
+  //FETCH VIDEOS ONLY THAT ARE SET isPublished as true
+  pipeline.push({
+    $match: {
+      isPublished: true,
+    },
+  });
+
+  //sortby can be views, createdAt,duration
+  //sortType can be ascending(-1) or descending(1)
+  if (sortBy && sortType) {
+    pipeline.push({
+      $sort: {
+        [sortBy]: sortType === "asc" ? 1 : -1,
+      },
+    });
+  } else {
+    pipeline.push({ $sort: { createdAt: -1 } });
+  }
+
+  pipeline.push(
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $projects: {
+              username: 1,
+              "avatar.url": 1,
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: "ownerDetails" }
+  );
+
+  const videoAggregate = Video.aggregate(pipeline);
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+
+  const video = await Video.aggregatePaginate(videoAggregate, options);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Videos fetched successfully"));
 });
+
+//get video, upload to cloudinary, create video
+const publishVideo = asyncHandler(async (req, res) => {
+  const { title, description } = req.body;
+
+  if ([title, description].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const videoFileLocalPath = req.files?.videoFile[0].path;
+  const thumbnailLocalPath = req.files?.thumbnail[0].path;
+
+  if (!videoFileLocalPath) {
+    throw new ApiError(400, "Video file local path is required");
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Thumbnail local file is required");
+  }
+
+  const videoFile = await uploadOnCloudinary(videoFileLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!videoFile) {
+    throw new ApiError(400, "Video file not found");
+  }
+
+  if (!thumbnail) {
+    throw new ApiError(400, "Thumbnail file not found");
+  }
+
+  const video = await Video.create({
+    title,
+    description,
+    duration: videoFile.duration,
+    videoFile: {
+      url: videoFile.url,
+      public_id: thumbnail.public_id,
+    },
+    owner: req.user?._id,
+    isPublished: true,
+  });
+
+  const videoUploaded = await Video.findById(video._id);
+
+  if (!videoUploaded) {
+    throw new ApiError(500, "Video Upload failed please try again");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video uploaded successfully"));
+});
+
+//get video by id
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  if (!isValidObjectId(req.user?._id)) {
+    throw new ApiError(400, "Invalid user Id");
+  }
+
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Schema.Types.ObjectId(videoId),
+      },
+    },
+  ]);
+});
+
 export { getAllVideos };
