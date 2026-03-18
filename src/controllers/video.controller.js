1c38ea662f526f523cfe8357243d5951041cfd60
07samir07 <samirbirua6738@gmail.com>
2026-03-18 21:36:43 +0530

    add deleteOnCloudinary function and update imports in video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Video } from "../models/video.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudinary.js";
 import { User } from "../models/user.model.js";
 import { Like } from "../models/like.model.js";
 
